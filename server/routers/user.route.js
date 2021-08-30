d05b124a5ba8a6d15ee82ed4c7f07bdf376be42a
Ayushmaan Verma <56289697+thatssoayushmaan@users.noreply.github.com>
2021-08-31 00:04:06 +0530

    Add files via upload

---
+++ server/routers/user.route.js
@@ -0,0 +1,61 @@
+const router = require('express').Router()
+
+const User = require('../Models/User')
+const Post = require('../Models/Post')
+
+const bcrypt = require('bcrypt')
+
+
+//Update
+router.put('/:id',async (req,res) => {
+    if (req.body.userId === req.params.id) {
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(req.body.password, salt);
+          }
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {$set: req.body}, {new:true})
+            res.status(200).json(updatedUser)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    } else {
+        res.status(401).json('You do not have access to this route')
+    }
+})
+//Delete
+router.delete('/:id', async(req,res) => {
+    if(req.body.userId === req.params.id){
+        try {
+            const user = await User.findById(req.params.id)
+            if(user){
+                try {
+                    await Post.deleteMany({username : user.username})
+                    await User.findByIdAndDelete(req.params.id)
+                    res.status(200).json("User has been deleted")
+                } catch (error) {
+                    res.status(500).json(error)
+                }
+            }else{
+                res.status(404).json('user not found')
+            }
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }else{
+        res.status(401).json('You do not have access to this route')
+    }
+})
+
+//GET
+router.get("/:id", async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      const { password, ...others } = user._doc;
+      res.status(200).json(others);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router
