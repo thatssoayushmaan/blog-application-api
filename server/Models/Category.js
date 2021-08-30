028755ad0b14ab92ce5f94d107e3af31e26b0cf5
Ayushmaan Verma <56289697+thatssoayushmaan@users.noreply.github.com>
2021-08-31 00:04:49 +0530

    Create Category.js

---
+++ server/Models/Category.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const CategorySchema = mongoose.Schema({
+    name: {
+        type :String,
+        required : true,
+        unique: true
+    }
+},
+{
+    timestamps : true
+})
+
+const Category = mongoose.model('Category', CategorySchema)
+
+module.exports = Category
